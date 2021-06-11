63075af4d12cf8679af1de1d73ae4f90b35dfad5
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 01:36:57 +0000

    create commentテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_010759) do
+ActiveRecord::Schema.define(version: 2021_06_11_013207) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "comment"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
