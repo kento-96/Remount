c2bd1f86f3449231149238ea423b159c1b3fca31
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 03:14:15 +0000

    create relationshipテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_030814) do
+ActiveRecord::Schema.define(version: 2021_06_11_031149) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2021_06_11_030814) do
     t.string "title"
     t.text "body"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
