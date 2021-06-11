433c03d249b1b9118bdfc53d6864a075446670ad
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 06:44:39 +0000

    create view file

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,24 @@ Rails.application.routes.draw do
  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
-  get "home/about"=>"homes#about"
-  
+  get "/about"=>"homes#about"
+
   resources :users ,only: [:show, :edit, :update] do
     collection do
       get "unsbscribe"
       patch "withdraw"
     end
-    
+
      resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
-  
+
+
   resources :posts do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  
+
   resources :mountains
 end
