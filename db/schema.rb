cf88b5c38b306c00975843fb744782f6bdd5679a
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 02:59:48 +0000

    create favoriteテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_013207) do
+ActiveRecord::Schema.define(version: 2021_06_11_013923) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.text "comment"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
