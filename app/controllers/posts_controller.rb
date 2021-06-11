bd4499114eb8a1f85d8ad3e31e54fed3fe506055
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 23:44:07 +0000

    create 新規投稿画面

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,17 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render "new"
+    end
   end
 
   def index
@@ -18,6 +27,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
   end
 
 end
