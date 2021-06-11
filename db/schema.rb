623fd1950c6af3bb3cc6b4acca769361fd484e71
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 03:10:56 +0000

    create postテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_030054) do
+ActiveRecord::Schema.define(version: 2021_06_11_030814) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2021_06_11_030054) do
     t.string "mountain_image_id"
     t.float "laitude"
     t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
