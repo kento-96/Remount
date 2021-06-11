d2bb3e536176d3fb8554a8a6830e96c1365826cf
kento-96 <kento.soccer.1107@gmail.com>
2021-06-11 06:07:11 +0000

    ルーティング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,controllers: {
+  registrations: "users/registrations"
+ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "homes#top"
+  get "home/about"=>"homes#about"
+  
+  resources :users ,only: [:show, :edit, :update] do
+    collection do
+      get "unsbscribe"
+      patch "withdraw"
+    end
+    
+     resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
+  
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
+  resources :mountains
 end
