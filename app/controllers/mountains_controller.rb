09ff72b4256a013e93f783c059038d212c59c726
kento-96 <kento.soccer.1107@gmail.com>
2021-06-12 07:15:53 +0000

    create 山新規投稿画面

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,8 +1,16 @@
 class MountainsController < ApplicationController
   def new
+    @mountain = Mountain.new
   end
 
   def create
+    @mountain = Mountain.new(mountain_params)
+    @mountain.user_id = current_user.id
+    if @mountain.save
+      redirect_to mountain_path(@mountain)
+    else
+      render "new"
+    end
   end
 
   def index
@@ -18,5 +26,9 @@ class MountainsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def park_params
+    params.require(:mountain).permit(:mountain_image, :mountain_name, :mountain_body, :address, :latitude, :longitude)
   end
 end
