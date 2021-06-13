505da1950f845645b7441b8f1080bcc75c877a4f
kento-96 <kento.soccer.1107@gmail.com>
2021-06-13 01:48:34 +0000

    create コメント機能、いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
+   attachment :image
+
   belongs_to :user
-  has_many :favorites, dependent: :destroy
   has_many :comments
-  attachment :image
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
