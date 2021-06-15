f27704091a3adac25776a80a78ded095d31520f3
kento-96 <kento.soccer.1107@gmail.com>
2021-06-15 06:28:29 +0000

    いいね、コメント非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,15 @@ class CommentsController < ApplicationController
     comment.post_id = @post.id
     comment.user_id = current_user.id
     comment.save
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
   end
 
   def destroy
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @post_comment = Comment.find(params[:id])
+    @post_comment.destroy
+    # Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    # redirect_to post_path(params[:post_id])
   end
 
   private
