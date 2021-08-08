2eafb5a0f7ffee509a94e9d1fc6dea3830e1de7c
kento-96 <kento.soccer.1107@gmail.com>
2021-08-08 08:37:31 +0000

    edit post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ApplicationRecord
    attachment :image
 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
 
